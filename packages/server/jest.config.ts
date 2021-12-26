247b633a2ccaea8fe3345e757aefa73c638b9ff6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2021-12-26 17:28:38 +0200

    Fixed jest config

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -5,6 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '@shared': '<rootDir>/../shared/src',
     '@classes/(.+)': '<rootDir>/src/app/classes/$1',
+    '@models/(.+)': '<rootDir>/src/app/models/$1',
     '@service/(.+)': '<rootDir>/src/app/modules/service/$1',
     '@routed/(.+)': '<rootDir>/src/app/modules/routed/$1',
     '@params': '<rootDir>/src/app/models/params',
