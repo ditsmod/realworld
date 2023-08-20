cf4f5823ac4c7f97aa91ead4dd6aa530d618b91a
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-08-20 23:03:11 +0300

    Fixed jest config

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -8,6 +8,9 @@ module.exports = {
     '@models/(.+)': '<rootDir>/src/app/models/$1',
     '@service/(.+)': '<rootDir>/src/app/modules/service/$1',
     '@routed/(.+)': '<rootDir>/src/app/modules/routed/$1',
+    '@configs/(.+)': '<rootDir>/src/app/configs/$1',
+    '@services-per-app/(.+)': '<rootDir>/src/app/services-per-app/$1',
+    '@utils/(.+)': '<rootDir>/src/app/utils/$1',
     '@params': '<rootDir>/src/app/models/params',
   },
 };
