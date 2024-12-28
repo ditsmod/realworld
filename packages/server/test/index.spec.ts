fad6b7769eaf6e68a71b3ae97fd2eee7a0f7280c
KostyaTretyak <ktretiak.in.ua@gmail.com>
2024-12-28 08:29:26 +0200

    Fixed tests.

--- packages/server/test/index.spec.ts
+++ packages/server/test/index.spec.ts
@@ -1,17 +1,20 @@
 import { createConnection } from 'mysql2/promise';
 import { TestApplication } from '@ditsmod/testing';
 import * as newman from 'newman';
-import { beforeAll, describe, expect } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { HttpServer } from '@ditsmod/core';
 
 import { AppModule } from '#app/app.module.js';
 import { MySqlConfigService } from '#service/mysql/mysql-config.service.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import postmanCollection from '#postman-collection';
-import { it } from 'node:test';
 
 describe('postman tests', () => {
+  let server: HttpServer;
+
   beforeAll(async () => {
+    server = await TestApplication.createTestApp(AppModule, { path: 'api' }).getServer();
     const config = new MySqlConfigService();
     const connection = await createConnection({ ...config, multipleStatements: true });
     await connection.query(`
@@ -26,14 +29,16 @@ describe('postman tests', () => {
     SET FOREIGN_KEY_CHECKS=1;`);
   });
 
-  it('run tests with newman', { timeout: 35000 }, (done) => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    TestApplication.createTestApp(AppModule, { path: 'api' })
-      .getServer()
-      .then((server) => {
-        expect.assertions(3);
-        const port = 3456;
+  afterAll(() => {
+    server?.close();
+  });
 
+  describe('run tests with newman', () => {
+    it('hello, world', () => {
+      expect.assertions(2);
+      const port = 3456;
+
+      return new Promise<void>((resolve, reject) => {
         server.listen(port, '0.0.0.0', () => {
           newman.run(
             {
@@ -48,13 +53,16 @@ describe('postman tests', () => {
               delayRequest: 0,
             },
             (err, summary) => {
-              expect(err).toBeFalsy();
+              if (err) {
+                reject(err);
+              }
               expect(summary.error).toBeFalsy();
               expect(summary.run.failures.length).toBe(0);
-              server.close(done);
+              resolve();
             }
           );
         });
       });
+    });
   });
 });
