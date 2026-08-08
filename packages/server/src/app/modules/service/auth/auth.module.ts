8951ba96a01e78af9fca8ffe40dd780427640c0b
KostyaTretyak <ktretiak.in.ua@gmail.com>
2026-08-08 12:24:38 +0300

    refactor: removed `@holu/authjs`

--- packages/server/src/app/modules/service/auth/auth.module.ts
+++ packages/server/src/app/modules/service/auth/auth.module.ts
@@ -1,24 +1,18 @@
 import { restModule } from '@holu/rest';
 import { JwtModule } from '@holu/jwt';
-import { AuthjsConfig, AuthjsModule } from '@holu/authjs';
 
 import { AuthService } from './auth.service.js';
 import { BearerGuard } from './bearer.guard.js';
 import { ModuleConfigService } from './config.service.js';
 import { CryptoService } from './crypto.service.js';
 import { PermissionsGuard } from './permissions.guard.js';
-import { OverriddenAuthConfig } from './authjs.config.js';
 
 const jwtModuleWithOpts = JwtModule.withOpts({ secret: process.env.JWT_SECRET, signOptions: { expiresIn: '1y' } });
-const authjs = AuthjsModule.withConfig({
-  token: AuthjsConfig,
-  useFactory: [OverriddenAuthConfig, OverriddenAuthConfig.prototype.initAuthjsConfig],
-});
 
 @restModule({
-  imports: [jwtModuleWithOpts, authjs],
+  imports: [jwtModuleWithOpts],
   providersPerMod: [ModuleConfigService],
   providersPerReq: [BearerGuard, CryptoService, AuthService, PermissionsGuard],
-  exports: [BearerGuard, CryptoService, AuthService, PermissionsGuard, jwtModuleWithOpts, authjs],
+  exports: [BearerGuard, CryptoService, AuthService, PermissionsGuard, jwtModuleWithOpts],
 })
 export class AuthModule {}
